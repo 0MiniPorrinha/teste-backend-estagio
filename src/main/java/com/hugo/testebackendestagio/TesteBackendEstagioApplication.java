ce630d7a716dbde692f695a2967d148ab16f228d
0MiniPorrinha <hugorodrigues969@gmail.com>
2022-07-18 09:32:46 -0300

    Iniciando projeto

---
+++ src/main/java/com/hugo/testebackendestagio/TesteBackendEstagioApplication.java
@@ -0,0 +1,13 @@
+package com.hugo.testebackendestagio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBackendEstagioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteBackendEstagioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugo/testebackendestagio/TesteBackendEstagioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.testebackendestagio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteBackendEstagioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
